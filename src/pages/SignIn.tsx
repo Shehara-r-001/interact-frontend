92dd263aa7c7344d9e73eeddb74230dd56b65788
Shehara Ranathisara <ranathisarashehara01@gmail.com>
2022-10-06 00:09:48 +0530

    handle login

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,45 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const [error, setError] = useState<string>();
+
+  const loginFunction: SubmitHandler<Inputs> = async (data) => {
+    const { email, password } = data;
+
+    await axios
+      .post('http://localhost:3333/api/user/login', {
+        email,
+        password,
+      })
+      .then(({ data }) => {
+        console.log(data);
+        setError('');
+      })
+      .catch((error) => {
+        setError(error.response.data.error);
+      })
+      .finally(() => {
+        setValue('email', '');
+        setValue('password', '');
+      });
+  };
+
   return (
     <div className='flex items-center justify-center h-screen w-full'>
       <div className='w-[50vw] md:w-[35vw] lg:w-[30vw] max-w-[300px] min-w-[260px] shadow-md hover:shadow-lg rounded-md'>
@@ -12,9 +51,27 @@ const SignIn = () => {
             let's just start where you left..
           </p>
         </div>
-        <form className='px-2 py-4 flex justify-center flex-col'>
-          <input type='text' placeholder='Email..' className='input_field' />
-          <input type='text' placeholder='Password..' className='input_field' />
+        <form
+          onSubmit={handleSubmit(loginFunction)}
+          className='px-2 py-4 flex justify-center flex-col'
+        >
+          <input
+            type='text'
+            placeholder='Email..'
+            className='input_field'
+            {...register('email', { required: true })}
+          />
+          {errors.email && <p className='form_error'>Email is required..!</p>}
+          <input
+            type='text'
+            placeholder='Password..'
+            className='input_field'
+            {...register('password', { required: true })}
+          />
+          {errors.password && (
+            <p className='form_error'>Password is required..!</p>
+          )}
+          {error && <p className='form_error mt-1'>{error}</p>}
           <button className='sign_btn'>SIGNIN</button>
           <p className='text-xs mt-4 px-1'>
             Don't have an account? SignUp from{' '}
